3c4783147b23a434697df66c8e511b39ff0c178a
Jen Cheshire <jen.cheshire@ons.gov.uk>
2023-02-24 10:58:20 +0000

    minor changes

--- film_app.py
+++ film_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.set_page_config(
     page_title="What shall we watch tonight?",
-    page_icon="🧛"
+    page_icon=":vampire:"
 )
 
 st.title("What shall we watch tonight?")
-st.write("Pick some specifics and I'll suggest a film")
+st.write("You can have any genre, as long as it's horror...")
 st.write("---")
 
 st.write("What are you in the mood for?")
