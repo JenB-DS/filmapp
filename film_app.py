c055e4fb83e111642884ec024308cf46cda0e5d2
Jen Cheshire <jen.cheshire@ons.gov.uk>
2023-02-24 10:46:21 +0000

    initial file

---
+++ film_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="What shall we watch tonight?",
+    page_icon="🧛"
+)
+
+st.title("What shall we watch tonight?")
+st.write("Pick some specifics and I'll suggest a film")
+st.write("---")
+
+st.write("What are you in the mood for?")
+ 
+operation = st.radio("Select any you fancy:",
+                    ("90s", "Slasher", "Comedy", "70s"))
+
+
+def pick_film():
+    return film
+
+
+if st.button("Suggest a film"):
+    pick_film()
