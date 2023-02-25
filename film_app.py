79c244607c87e4527820ec0ebd4da9da5fef382d
Jen Cheshire <jen.cheshire@ons.gov.uk>
2023-02-25 08:56:28 +0000

    minor changes

--- film_app.py
+++ film_app.py
@@ -12,10 +12,10 @@ st.write("---")
 st.write("What are you in the mood for?")
  
 operation = st.radio("Select any you fancy:",
-                    ("Slasher", "Comedy",))
+                    ("Horror", "Comedy", "Action"))
 
-operation1 = st.radio("Era:",
-                    ("90s", "70s"))
+operation1 = st.radio("How many suggestions:",
+                    ("1", "3", "all"))
 
 
 def pick_film():
