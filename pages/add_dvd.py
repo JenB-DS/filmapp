dfdba257d3351958f286bba1a5b3d76d932ed879
JenB-DS <burton.cheshire@gmail.com>
2023-03-04 11:20:16 +0000

    adding add dvd

--- pages/add_dvd.py
+++ pages/add_dvd.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import csv
 
 
 
@@ -13,24 +13,20 @@ st.write("Add a DVD to database")
 st.write("---")
 
 
+Title = st.text_input("Title: ")
 
-new_entry = {}
-new_entry["Title"] = st.text_input("Title: ")
-
-new_entry["Type"] = st.radio("Film or TV series?",
+Type = st.radio("Film or TV series?",
                           ("Film", "Television"), value=False)
 
-new_entry["Genre"] = st.selectbox("Genre:",
+Genre = st.selectbox("Genre:",
                                ("Comedy", "Family", "Horror", "Sci-Fi", "Western"), value=False)
 
-new_entry["Decade"] = st.selectbox("Decade:",
+Decade = st.selectbox("Decade:",
                              ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"), value=False)
 
-def add_dvd():
-
-    st.success("DVD added")
-
-
-if st.button("Add DVD"):
-    add_dvd()
-
+if st.button('Add DVD'):
+    # Write the user-submitted data to a CSV file
+    with open('newdvd.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([Title, Type, Genre, Decade])
+    st.success('DVD added')
