8eb856ce783820db5c09f4a1a563d15e603ad31d
JenB-DS <burton.cheshire@gmail.com>
2023-03-04 10:55:09 +0000

    adding add dvd

--- pages/add_dvd.py
+++ pages/add_dvd.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import pandas as pd
-from git import Repo
 
 
-email = "burton.cheshire@gmail.com"
-username = "JenB-DS"
 
-Repo().git.config("--global", "user.email", email)
-Repo().git.config("--global", "user.name", username)
+
 
 st.markdown("# Add a DVD")
 st.sidebar.markdown("# Add a DVD")
@@ -31,14 +27,7 @@ new_entry["Decade"] = st.selectbox("Decade:",
                              ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"))
 
 def add_dvd():
-    url = "https://raw.githubusercontent.com/JenB-DS/filmapp/main/dvd_collection.csv"
-    df = pd.read_csv(url)
-    df = df.append(new_entry, ignore_index=True)
-    df.to_csv('dvd_collection.csv', index=False)
-    repo = Repo()
-    repo.git.add('dvd_collection.csv')
-    repo.git.commit('-m', 'Update data.csv')
-    repo.git.push()
+
     st.success("DVD added")
 
 
