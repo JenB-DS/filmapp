136a1b3cf3ee71deb6a1501b1d0143dcb2967b6a
JenB-DS <burton.cheshire@gmail.com>
2023-03-04 11:21:16 +0000

    adding add dvd

--- pages/add_dvd.py
+++ pages/add_dvd.py
@@ -22,7 +22,7 @@ Genre = st.selectbox("Genre:",
                                ("Comedy", "Family", "Horror", "Sci-Fi", "Western"))
 
 Decade = st.selectbox("Decade:",
-                             ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"), value=False)
+                             ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"))
 
 if st.button('Add DVD'):
     # Write the user-submitted data to a CSV file
