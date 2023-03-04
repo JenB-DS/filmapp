c339428c6d1a5e7c83d9e72165dbfe58dfc6598b
JenB-DS <burton.cheshire@gmail.com>
2023-03-04 11:04:41 +0000

    adding add dvd

--- pages/add_dvd.py
+++ pages/add_dvd.py
@@ -18,13 +18,13 @@ new_entry = {}
 new_entry["Title"] = st.text_input("Title: ")
 
 new_entry["Type"] = st.radio("Film or TV series?",
-                          ("Film", "Television"))
+                          ("Film", "Television"), value=False)
 
 new_entry["Genre"] = st.selectbox("Genre:",
-                               ("Comedy", "Family", "Horror", "Sci-Fi", "Western"))
+                               ("Comedy", "Family", "Horror", "Sci-Fi", "Western"), value=False)
 
 new_entry["Decade"] = st.selectbox("Decade:",
-                             ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"))
+                             ("2020s", "2010s", "2000s" "1990s", "1980s", "1970s", "1960s", "1950s", "1940s", "1930s", "Not sure"), value=False)
 
 def add_dvd():
 
