6d203b5a75af0d3e0ed897a9be2b58644e7c1a23
JenB-DS <burton.cheshire@gmail.com>
2023-08-02 21:04:37 +0100

    add new page

--- pages/choose_dvd.py
+++ pages/choose_dvd.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 # Backend storage (you can replace this with a database)
 votes = {'option1': 0, 'option2': 0, 'option3': 0, 'option4': 0}
+total_votes = 0
 
 # Streamlit app
 def main():
+    global total_votes
     st.title('Voting App')
     
     # Display voting options and buttons
@@ -12,12 +14,12 @@ def main():
     for option in votes.keys():
         if st.button(option):
             votes[option] += 1
+            total_votes += 1
 
-    # Calculate the winner
-    winner = max(votes, key=votes.get)
-
-    # Display the winner
-    st.subheader(f'The current winner is: {winner}')
+    # Show the winner when there are four votes
+    if total_votes == 4:
+        winner = max(votes, key=votes.get)
+        st.subheader(f'The winner is: {winner}')
 
 if __name__ == '__main__':
     main()
