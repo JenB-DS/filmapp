6b5eb4333167d25e86f4ded2d475fdd492007098
JenB-DS <burton.cheshire@gmail.com>
2023-08-02 21:12:52 +0100

    add new page

--- pages/choose_dvd.py
+++ pages/choose_dvd.py
@@ -1,7 +1,35 @@
 import streamlit as st
+import random
+import requests
+import io
+import csv
+
+url = "https://raw.githubusercontent.com/JenB-DS/filmapp/main/dvd_collection.csv"
+response = requests.get(url)
+csv_data = response.content.decode("utf-8")
+csv_file = io.StringIO(csv_data)
+
+reader = csv.reader(csv_file)
+film_list = []
+
+
+for row in reader:
+    if row[1] == "Film" and row[2] == "Horror":
+        film_list.append(row[0])
+
+nums = []
+while len(nums) < 3:
+    n = random.randint(0, len(film_list) - 1)
+    if n not in nums:
+        nums.append(n)
+    else:
+        # Replace duplicate with a new random number
+        n = random.randint(0, len(film_list) - 1)
+
+        shortlist = [film_list[nums[0]], film_list[nums[1]], film_list[nums[2]]]
 
 # Backend storage (you can replace this with a database)
-votes = {'option1': 0, 'option2': 0, 'option3': 0, 'option4': 0}
+votes = {shortlist[0]: 0, shortlist[1]: 0, shortlist[2]: 0}
 total_votes = 0
 
 # Streamlit app
